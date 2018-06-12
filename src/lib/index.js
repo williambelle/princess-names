a0e85e89f29071a9d6afdfd704a43956d1b0ec2e
William Belle <william.belle@gmail.com>
2018-06-12 18:56:15 +0200

    move to eslint

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,14 +2,14 @@
 
 var princessNames = require('./princess-names.json');
 
-var randomItem = function(arr) {
+var randomItem = function (arr) {
   if (!Array.isArray(arr)) {
     throw new TypeError('Expected an array');
   }
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-exports.all    = princessNames;
-exports.random = function() {
+exports.all = princessNames;
+exports.random = function () {
   return randomItem(princessNames);
 };
--- test/test-princess-names.js
+++ test/test-princess-names.js
@@ -1,43 +1,38 @@
-(function(){
-  'use strict';
+var should = require('chai').should();
+var princessNames = require('../src/lib/index.js');
 
-  var should        = require('chai').should();
-  var princessNames = require('../src/lib/index.js');
-
-  describe('princess-names', function(){
-    it('should contains at least 10 items', function(){
-      var enoughNames = Object.keys(princessNames.all).length > 9;
-      enoughNames.should.equal(true);
-    });
-
-    it('should return a princess name', function(){
-      var princess = princessNames.random();
-      var enoughLength = princess.length > 2;
-      princess.should.be.a('string');
-      enoughLength.should.equal(true);
-    });
-
-    it('should return Aire', function(){
-      var allNames = princessNames.all;
-      should.equal(allNames[0], 'Adora');
-    });
-
-    it('should return undefined', function(){
-      var allNames = princessNames.all;
-      should.equal(allNames[10000], undefined);
-    });
+describe('princess-names', function () {
+  it('should contains at least 10 items', function () {
+    var enoughNames = Object.keys(princessNames.all).length > 9;
+    enoughNames.should.equal(true);
   });
 
-  var rewire   = require("rewire");
-  var pNRewire = rewire('../src/lib/index.js');
-
-  describe('princess-names-rewire', function(){
-    it('should throw an exception', function(){
-      (function() {
-        pNRewire.__set__('princessNames', {});
-        pNRewire.random();
-      }).should.throw('Expected an array');
-    });
+  it('should return a princess name', function () {
+    var princess = princessNames.random();
+    var enoughLength = princess.length > 2;
+    princess.should.be.a('string');
+    enoughLength.should.equal(true);
   });
 
-})();
+  it('should return Aire', function () {
+    var allNames = princessNames.all;
+    should.equal(allNames[0], 'Adora');
+  });
+
+  it('should return undefined', function () {
+    var allNames = princessNames.all;
+    should.equal(allNames[10000], undefined);
+  });
+});
+
+var rewire = require('rewire');
+var pNRewire = rewire('../src/lib/index.js');
+
+describe('princess-names-rewire', function () {
+  it('should throw an exception', function () {
+    (function () {
+      pNRewire.__set__('princessNames', {});
+      pNRewire.random();
+    }).should.throw('Expected an array');
+  });
+});
